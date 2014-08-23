1e1fb426f381941796f5606aeed30aa7f2b8f869
Toni <toni.schindler@gmail.com>
2014-08-23 17:53:32 -0400

    added method to delete event handlers

--- scripts/utilities/eventhandlerlist.js
+++ scripts/utilities/eventhandlerlist.js
@@ -8,6 +8,11 @@ EventHandlerList.prototype.add = function (handler) {
     this.handlers.add(handler);
 };
 
+EventHandlerList.prototype.delete = function (handler) {
+    'use strict';
+    this.handlers.delete(handler);
+};
+
 EventHandlerList.prototype.fire = function (data) {
     'use strict';
     this.handlers.forEach(function (h) {
