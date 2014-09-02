b6dec12951a36f5cd916a2ed022af87e175b569c
Toni <toni.schindler@gmail.com>
2014-09-02 00:12:32 -0500

    speed up getting a small random sample from a big array

--- scripts/utilities/random.js
+++ scripts/utilities/random.js
@@ -12,8 +12,27 @@ function randomChoice(list) {
     return arr[randomInteger(0, arr.length - 1)];
 }
 
+// use when n much smaller than arr.length
+function smallRandomSample(arr, n) {
+    'use strict';
+    var sample = [],
+        used = [],
+        r;
+    while (sample.length < Math.min(arr.length, n)) {
+        r = randomInteger(0, arr.length - 1);
+        if (used.indexOf(r) === -1) {
+            used.push(r);
+            sample.push(arr[r]);
+        }
+    }
+    return sample;
+}
+
 function randomSample(list, n) {
     'use strict';
+    if (list instanceof Array && n <= list.length / 10) {
+        return smallRandomSample(list, n);
+    }
     var sample = [],
         remaining = list instanceof Array ? list.length : list.size;
     list.forEach(function (e) {
